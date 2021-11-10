65b8ccebd0540aebdced772e9092c0f4cf2237b1
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2021-11-10 22:31:21 +0700

    Add openapi doccument details

--- app/routers/service_site.py
+++ app/routers/service_site.py
@@ -3,133 +3,181 @@ from fastapi import APIRouter, HTTPException
 from typing import Optional
 
 from app.utils.paginator import Paginator
-from app.database import add_site, delete_site, retrieve_site, retrived_sites, update_site
-from app.models.site import Site
+from app.database import add_site, delete_site, retrieve_site, retrive_sites, update_site
+from app.models.site import Site, GetSitesResponse, Message
 
 router = APIRouter(
     tags=["service site"]
 )
 
 
-@router.get("/sites", response_description="Service sites retrived")
+@router.get("/sites",
+            response_description="Service sites retrived",
+            summary="Get every service sites",
+            response_model=GetSitesResponse,
+            responses={
+                404: {"model": Message, "description": "Not found"},
+                200: {"description": "Found a service site",
+                      "content": {
+                          "application/json": {
+                              "example": {"response": [{"name": "service site 1", "location": "service site 1's location"}, {"name": "service site 2", "location": "service site 2's location"}, {"name": "service site 3", "location": "service site 3's location"}]}
+                          }
+                      }}
+            }
+            )
 async def read_site_names(
     limit: Optional[int] = None,
     page: Optional[int] = 1
 ):
     """
-        Show service sites information:
+        ## Show service sites information:
 
         - **limit** : number of users to be shown as a result
         - **page** : number of pages to be shown as a result
 
     """
-    sites = await retrived_sites()
+    sites = await retrive_sites()
     if sites:
         site_paginator = Paginator(sites)
         site_paginator.paginate(page=page, limit=limit)
 
-        return {
-            "response": {
-                "service site data": site_paginator.get_page_data(),
-                "service sites": site_paginator.get_items()
-            }
+        return{
+            "response": site_paginator.get_items()
         }
+        # return {
+        #     "response": {
+        #         "page_data": site_paginator.get_page_data(),
+        #         "service_sites": site_paginator.get_items()
+        #     }
+        # }
 
 
-@router.get("/site/{id}", response_description="Service site data retrived")
+@router.get("/site/{id}",
+            response_description="Service site data retrived",
+            summary="Get a service site info by id",
+            response_model=Site,
+            responses={
+                404: {"model": Message, "description": "Not found"},
+                200: {"description": "Found a service site",
+                      "content": {
+                          "application/json": {
+                              "example": {"response": {"name": "service site", "location": "location"}}
+                          }
+                      }}
+            }
+            )
 async def read_one_site(
     id: str
 ):
     """
-        Show a service site information:
+        ## Show a service site information:
 
-        - **site_id** : service site id
+        - **id** : service site id
     """
     site = await retrieve_site(id)
     if site:
         return {
             "response": {
-                "message": "found a service site",
-                "service site": {
-                    "name": site["name"],
-                    "location": site["location"]
-                }
+                "name": site["name"],
+                "location": site["location"]
             }
         }
     raise HTTPException(
         status_code=404, detail="service site is not found")
 
 
-@router.post("/site", response_description="Service site data added into the database", status_code=201)
+@router.post("/site",
+             response_description="Service site data added into the database",
+             summary="Create a new service site",
+             status_code=201,
+             response_model=Message,
+             responses={
+                 201: {"description": "Create Successfull",
+                                      "content": {
+                                          "application/json": {
+                                              "example": {"response": "create site id site_id success"}
+                                          }
+                                      }}
+             }
+             )
 async def add_site_data(
-    name: str,
-    location: str
+    site: Site,
 ):
     """
-        Create a new service site:
+        ## Create a new service site:
 
         - **name** : service site name
         - **location** : service site location
     """
-    site = {"name": name, "location": location}
-    new_site = await add_site(site)
+    result = dict(**site.dict())
+    new_site = await add_site(result)
     return {
-        "response": {
-            "message": "create site successfully.",
-        }
+        "response": new_site
     }
 
 
-@router.put("/site/{id}")
+@router.put("/site/{id}",
+            response_description="Service site data updated into the database",
+            summary="Update a service site",
+            response_model=Message,
+            responses={
+                404: {"model": Message, "description": "Not found"},
+                200: {"description": "Update Successfull",
+                      "content": {
+                          "application/json": {
+                              "example": {"response": "update id site_id success"}
+                          }
+                      }}
+            }
+            )
 async def updated_site(
     id: str,
-    name: Optional[str] = "",
-    location: Optional[str] = "",
+    site: Site,
 ):
     """
-        Update a service site information:
+        ## Update a service site information:
 
-        - **site_id** : service site deleted id
-        - **site_name** : service site name
-        - **new_name** : a new service site name
-        - **new_location** : a new service site location
+        - **id** : service site deleted id
+        - **name** : a new service site name
+        - **location** : a new service site location
     """
-
-    new_value = {
-        "name": name,
-        "location": location
-    }
-    if not name:
-        new_value.pop("name")
-    if not location:
-        new_value.pop("location")
+    new_value = dict(**site.dict())
 
     updated_site = await update_site(id, new_value)
     if updated_site:
         return{
-            "response": {
-                "message": f"update {id} success"
-            }
+            "response": f"update {id} success"
         }
     raise HTTPException(
         status_code=404, detail=f"service site {id} not found")
 
 
-@router.delete("/site/{id}", response_description="Service site data deleted from the database")
+@router.delete("/site/{id}",
+               #    response_description="Service site data deleted from the database",
+               summary="Remove a service site",
+               response_model=Message,
+               responses={
+                   404: {"model": Message, "description": "Not found"},
+                   200: {"description": "Remove Successfull",
+                         "content": {
+                             "application/json": {
+                                 "example": {"response": "delete site id site_id success"}
+                             }
+                         }}
+               }
+               )
 async def remove_site(
     id: str
 ):
     """
-        Remove a service site information:
+        ## Remove a service site information:
 
-        - **site_name** : service site deleted name
+        - **id** : an id of service site deleted
     """
     deleted_site = await delete_site(id)
     if deleted_site:
         return {
-            "response": {
-                "message": f"delete success"
-            }
+            "response": f"delete site id {id} success"
         }
     raise HTTPException(
         status_code=404, detail=f"service site {id} not found")
