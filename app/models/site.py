fee246e6317e1831d67e0744941f5e265dccecc7
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2021-11-16 13:29:13 +0700

    change pydantic model for response

--- app/models/site.py
+++ app/models/site.py
@@ -1,5 +1,6 @@
 from typing import List, Dict, Optional
 from pydantic import BaseModel, Extra, create_model
+from starlette import responses
 
 
 class Site(BaseModel):
@@ -27,6 +28,10 @@ class PageData(BaseModel):
 
 class GetSitesResponse(BaseModel):
     response: List[Site]
+
+
+class GetSiteResponse(BaseModel):
+    response: Site
 
 
 class Message(BaseModel):
