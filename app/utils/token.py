0dda33d6b092a017bc408421cf787f5bcb86bd8e
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2021-11-23 15:50:50 +0700

    add return statement to token data

--- app/utils/token.py
+++ app/utils/token.py
@@ -26,5 +26,6 @@ def verify_token(token: str, credentials_exception):
         if username is None:
             raise credentials_exception
         token_data = TokenData(username=username)
+        return token_data
     except JWTError:
         raise credentials_exception
